b0a12bacb6b3943e438321192c8795d7d61de805
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2024-10-09 12:27:19 -0400

    Initial

---
+++ QuantileQuantilePlugin.R
@@ -0,0 +1,20 @@
+### R code from vignette source 'EBSeq_Vignette.Rnw'
+
+###################################################
+### code chunk number 1: EBSeq_Vignette.Rnw:172-173
+###################################################
+library(EBSeq)
+
+
+input <- function(inputfile) {
+   EBOut <<- readRDS(inputfile)
+}
+
+run <- function() {}
+
+output <- function(outputfile) {
+#par(mfrow=c(1,2))
+pdf(outputfile)
+QQP(EBOut)
+}
+
